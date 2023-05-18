1f8dc8e358d2981a698c4196157d59f4a68e27c8
sairam566 <sairam1431995@gmail.com>
2023-05-18 10:38:30 +0530

    Message Formatters

---
+++ 1_Spring_Beans/src/main/java/com/tsr/beans/HTMLMessageFormatterImpl.java
@@ -0,0 +1,10 @@
+package com.tsr.beans;
+
+public class HTMLMessageFormatterImpl implements IMessageFormatter{
+
+	@Override
+	public String formatMessage(String message) {
+		return "<html><body>"+message+"</body></html>";
+	}
+
+}
