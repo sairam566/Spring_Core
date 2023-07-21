ee0beac6d248bd9625d15b912e38b468ab658527
sairam566 <sairam1431995@gmail.com>
2023-07-21 11:26:36 +0530

    Main

---
+++ 26_Bean_PostProcessor/src/main/java/com/tsr/App.java
@@ -0,0 +1,40 @@
+package com.tsr;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionReader;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+import com.tsr.beans.GasStation;
+import com.tsr.beans.Guarage;
+import com.tsr.beans.ObjectTickerBeanPostProcessor;
+import com.tsr.counter.ObjectTracker;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+    	// Using Bean Definition
+//        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+//        
+//        BeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+//        
+//        reader.loadBeanDefinitions(new ClassPathResource("com/tsr/config/application-context.xml"));
+    	
+    	ApplicationContext beanFactory = new ClassPathXmlApplicationContext("com/tsr/config/application-context.xml");
+        
+        //ObjectTickerBeanPostProcessor postProcessor = beanFactory.getBean("objectTickerBeanPostProcessor", ObjectTickerBeanPostProcessor.class);
+        
+        //beanFactory.addBeanPostProcessor(postProcessor);
+        
+        beanFactory.getBean("gasStation",GasStation.class);
+        beanFactory.getBean("guarage",Guarage.class);
+        
+        System.out.println(ObjectTracker.getCounter());
+    }
+}
