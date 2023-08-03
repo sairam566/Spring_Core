51c1e736c84041f4a500ceab37d7cb2a7bdc2cc6
sairam566 <sairam1431995@gmail.com>
2023-08-03 09:33:40 +0530

    corrected a comment

--- 30_Stereotype_Annotations/src/main/java/com/tsr/beans/combination2/Room.java
+++ 30_Stereotype_Annotations/src/main/java/com/tsr/beans/combination2/Room.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 //Have Source Code
-// Target
+// Dependent
 @Component
 public class Room {
 	@Value("566")
